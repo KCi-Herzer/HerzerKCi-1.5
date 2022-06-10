305d4eb29058dfd9ca7f9a6a6ceb931522bb103a
neonhippie_kci <neonhippie.kci@gmail.com>
2022-06-09 21:39:25 -0700

    Added buttons with some sizer elements.

--- HerzerKCi-1.5/mainFrame.cpp
+++ HerzerKCi-1.5/mainFrame.cpp
@@ -6,7 +6,30 @@ wxEND_EVENT_TABLE()
 
 mainFrame::mainFrame() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(560, 240), wxSize(540, 495))
 {
+	textBox = new wxTextCtrl(this, wxID_ANY, "0", wxPoint(wxDefaultPosition), wxSize(560, 90), wxTE_READONLY);	//
+	textBox->SetAutoLayout(true);
+	/*wxFo
+	textBox->SetFont*/
+	calcButtons = new wxButton * [buttonsX * buttonsY];
+	wxGridSizer* buttonSizer = new wxGridSizer(buttonsX, buttonsY, 0, 0);
+	wxFlexGridSizer* calcSizer = new wxFlexGridSizer(1);
 	
+	for (int x = 0; x < buttonsX; x++)
+	{
+		for (int y = 0; y < buttonsY; y++)
+		{
+			calcButtons[y * buttonsX + x] = new wxButton(this, 10000 + (y * buttonsX + x));
+			buttonSizer->Add(calcButtons[y * buttonsX + x], 1, wxEXPAND | wxALL);
+		}
+	}
+	/*this->SetSizer(buttonSizer);
+	buttonSizer->Layout();*/
+	calcSizer->AddGrowableCol(0);
+	calcSizer->AddGrowableRow(1);
+	calcSizer->Add(textBox, 1, wxEXPAND | wxALL);
+	calcSizer->Add(buttonSizer, 1, wxEXPAND | wxALL);
+	this->SetSizer(calcSizer);
+	calcSizer->Layout();
 }
 
 mainFrame::~mainFrame()
--- HerzerKCi-1.5/mainFrame.h
+++ HerzerKCi-1.5/mainFrame.h
@@ -6,7 +6,10 @@ class mainFrame : public wxFrame
 public:
 	mainFrame();
 	~mainFrame();
-
+	wxTextCtrl* textBox = nullptr;
+	wxButton** calcButtons;
+	int buttonsX = 4;
+	int buttonsY = 6;
 	
 
 	wxDECLARE_EVENT_TABLE();
