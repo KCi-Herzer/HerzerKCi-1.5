2e67db7fe408df36ac7fcc83acd70fefb2dbe007
KCi-Herzer <neonhippie.kci@gmail.com>
2022-06-18 09:04:07 -0700

    Error LNK2001 Fix: Removed CalculatorProcessor.cpp

--- HerzerKCi-1.5/CalculatorProcessor.h
+++ HerzerKCi-1.5/CalculatorProcessor.h
@@ -4,17 +4,56 @@
 class CalculatorProcessor
 {
 private:
+	CalculatorProcessor() {}
 	static CalculatorProcessor* _processor;
 	int baseNumber = 0;
-	CalculatorProcessor() {}
+	
 public:
-	static CalculatorProcessor* GetInstance(); //Most important part of a singleton
-	void SetBaseNumber(int _baseNumber);
+	static CalculatorProcessor* GetInstance() //Most important part of a singleton
+	{
+		if (_processor == nullptr)
+		{
+			_processor = new CalculatorProcessor();
+		}
+		return _processor;
+	}
+
+	void SetBaseNumber(int _baseNumber)
+	{
+		baseNumber = _baseNumber;
+	}
 
 	CalculatorProcessor(CalculatorProcessor& other) = delete; //Makes it so we can't call the copy constructor
 	void operator = (const CalculatorProcessor& other) = delete;
-	std::string GetDecimal();
-	std::string GetBinary();
-	std::string GetHexadecimal();
+	
+	std::string GetDecimal()
+	{
+		return std::to_string(baseNumber);
+	}
+
+	std::string GetHexadecimal()
+	{
+		return std::string();
+	}
+
+	std::string GetBinary()
+	{
+		std::string result = "";
+		int number = baseNumber;
+		for (int i = 0; i < 32; i++)
+		{
+			if (baseNumber % 2 == 0)
+			{
+				result = "0" + result;
+			}
+			else
+			{
+				result = "1" + result;
+			}
+			number = number / 2;
+		}
+
+		return result;
+	}
 };
 
