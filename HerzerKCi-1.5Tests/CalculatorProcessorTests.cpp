3ae0edb5f64b5820e45974dc9cd15507bf9a591b
KCi Herzer <neonhippie.kci@gmail.com>
2022-06-24 11:36:25 -0400

    Re Added both test files, build successful.

--- HerzerKCi-1.5Tests/CalculatorProcessorTests.cpp
+++ HerzerKCi-1.5Tests/CalculatorProcessorTests.cpp
@@ -5,7 +5,7 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace HerzerKCi15Tests
 {
-	TEST_CLASS(HerzerKCi15Tests)
+	TEST_CLASS(CalculatorProcessorTests)
 	{
 	public:
 		
