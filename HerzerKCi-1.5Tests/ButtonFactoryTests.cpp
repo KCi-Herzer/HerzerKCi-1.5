9db20c997a7d82a3ce0bbc65c6411b6964431609
KCi Herzer <neonhippie.kci@gmail.com>
2022-06-24 12:49:54 -0400

    Implemented first button test

--- HerzerKCi-1.5Tests/ButtonFactoryTests.cpp
+++ HerzerKCi-1.5Tests/ButtonFactoryTests.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "../HerzerKCi-1.5/ButtonFactory.h"
+#include "../HerzerKCi-1.5/mainFrame.h"
+#include "../HerzerKCi-1.5/mainFrame.cpp"
+#include "../HerzerKCi-1.5/ButtonFactory.cpp"
 
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -10,10 +13,67 @@ namespace HerzerKCi15Tests
 	TEST_CLASS(ButtonFactoryTests)
 	{
 	public:
-
-		TEST_METHOD(TestMethod1)
+		mainFrame* testFrame = new mainFrame();
+		ButtonFactory* factory = new ButtonFactory();
+		TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //1
 		{
-
+			factory->CreateButtons(testFrame);
+			int numOfButtons = factory->buttonSizer->GetItemCount();
+			Assert::AreEqual(24, numOfButtons);
 		}
+		//TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //2
+		//{
+		//	factory->CreateButtons(testFrame);
+		//	int numOfButtons = factory->buttonSizer->GetItemCount();
+		//	Assert::AreEqual(24, numOfButtons);
+		//}
+		//TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //3
+		//{
+		//	factory->CreateButtons(testFrame);
+		//	int numOfButtons = factory->buttonSizer->GetItemCount();
+		//	Assert::AreEqual(24, numOfButtons);
+		//}
+		//TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //4
+		//{
+		//	factory->CreateButtons(testFrame);
+		//	int numOfButtons = factory->buttonSizer->GetItemCount();
+		//	Assert::AreEqual(24, numOfButtons);
+		//}
+		//TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //5
+		//{
+		//	factory->CreateButtons(testFrame);
+		//	int numOfButtons = factory->buttonSizer->GetItemCount();
+		//	Assert::AreEqual(24, numOfButtons);
+		//}
+		//TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //6
+		//{
+		//	factory->CreateButtons(testFrame);
+		//	int numOfButtons = factory->buttonSizer->GetItemCount();
+		//	Assert::AreEqual(24, numOfButtons);
+		//}
+		//TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //7
+		//{
+		//	factory->CreateButtons(testFrame);
+		//	int numOfButtons = factory->buttonSizer->GetItemCount();
+		//	Assert::AreEqual(24, numOfButtons);
+		//}
+		//TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //8
+		//{
+		//	factory->CreateButtons(testFrame);
+		//	int numOfButtons = factory->buttonSizer->GetItemCount();
+		//	Assert::AreEqual(24, numOfButtons);
+		//}
+		//TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //9
+		//{
+		//	factory->CreateButtons(testFrame);
+		//	int numOfButtons = factory->buttonSizer->GetItemCount();
+		//	Assert::AreEqual(24, numOfButtons);
+		//}
+		//TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //10
+		//{
+		//	factory->CreateButtons(testFrame);
+		//	int numOfButtons = factory->buttonSizer->GetItemCount();
+		//	Assert::AreEqual(24, numOfButtons);
+		//}
 	};
 }
