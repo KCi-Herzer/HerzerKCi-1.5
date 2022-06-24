ac42417ca342ccdee491cff4a9007825ac305550
KCi Herzer <neonhippie.kci@gmail.com>
2022-06-24 13:12:25 -0400

    Finished Implementing ButtonFactoryTests

--- HerzerKCi-1.5Tests/ButtonFactoryTests.cpp
+++ HerzerKCi-1.5Tests/ButtonFactoryTests.cpp
@@ -21,59 +21,53 @@ namespace HerzerKCi15Tests
 			int numOfButtons = factory->buttonSizer->GetItemCount();
 			Assert::AreEqual(24, numOfButtons);
 		}
-		//TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //2
-		//{
-		//	factory->CreateButtons(testFrame);
-		//	int numOfButtons = factory->buttonSizer->GetItemCount();
-		//	Assert::AreEqual(24, numOfButtons);
-		//}
-		//TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //3
-		//{
-		//	factory->CreateButtons(testFrame);
-		//	int numOfButtons = factory->buttonSizer->GetItemCount();
-		//	Assert::AreEqual(24, numOfButtons);
-		//}
-		//TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //4
-		//{
-		//	factory->CreateButtons(testFrame);
-		//	int numOfButtons = factory->buttonSizer->GetItemCount();
-		//	Assert::AreEqual(24, numOfButtons);
-		//}
-		//TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //5
-		//{
-		//	factory->CreateButtons(testFrame);
-		//	int numOfButtons = factory->buttonSizer->GetItemCount();
-		//	Assert::AreEqual(24, numOfButtons);
-		//}
-		//TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //6
-		//{
-		//	factory->CreateButtons(testFrame);
-		//	int numOfButtons = factory->buttonSizer->GetItemCount();
-		//	Assert::AreEqual(24, numOfButtons);
-		//}
-		//TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //7
-		//{
-		//	factory->CreateButtons(testFrame);
-		//	int numOfButtons = factory->buttonSizer->GetItemCount();
-		//	Assert::AreEqual(24, numOfButtons);
-		//}
-		//TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //8
-		//{
-		//	factory->CreateButtons(testFrame);
-		//	int numOfButtons = factory->buttonSizer->GetItemCount();
-		//	Assert::AreEqual(24, numOfButtons);
-		//}
-		//TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //9
-		//{
-		//	factory->CreateButtons(testFrame);
-		//	int numOfButtons = factory->buttonSizer->GetItemCount();
-		//	Assert::AreEqual(24, numOfButtons);
-		//}
-		//TEST_METHOD(EnsureCorrectAmountOfButtonsAreMade) //10
-		//{
-		//	factory->CreateButtons(testFrame);
-		//	int numOfButtons = factory->buttonSizer->GetItemCount();
-		//	Assert::AreEqual(24, numOfButtons);
-		//}
+		TEST_METHOD(EnsureUnusedButton1IsDisabled) //2
+		{
+			factory->CreateButtons(testFrame);
+			Assert::IsFalse(factory->calcButtons[3]->IsEnabled());
+		}
+		TEST_METHOD(EnsureUnusedButton2IsDisabled) //3
+		{
+			factory->CreateButtons(testFrame);
+			Assert::IsFalse(factory->calcButtons[11]->IsEnabled());
+		}
+		TEST_METHOD(EnsureUnusedButton3IsDisabled) //4
+		{
+			factory->CreateButtons(testFrame);
+			Assert::IsFalse(factory->calcButtons[22]->IsEnabled());
+		}
+		TEST_METHOD(EnsureLableForEqualsIsCorrect) //5
+		{
+			factory->CreateButtons(testFrame);
+			Assert::AreEqual("=", factory->calcButtons[23]->GetLabelText());
+		}
+		TEST_METHOD(EnsureLableForHexIsCorrect) //6
+		{
+			factory->CreateButtons(testFrame);
+			Assert::AreEqual("(hex)", factory->calcButtons[17]->GetLabelText());
+		}
+		TEST_METHOD(EnsureLableForSevenIsNotOne) //7
+		{
+			factory->CreateButtons(testFrame);
+			Assert::AreNotEqual("1", factory->calcButtons[0]->GetLabelText());
+		}
+		TEST_METHOD(EnsureLableForZeroIsNotSeven) //8
+		{
+			factory->CreateButtons(testFrame);
+			Assert::AreNotEqual("7", factory->calcButtons[7]->GetLabelText());
+		}
+		TEST_METHOD(EnsureButtonIdOfMinusIsCorrect) //9
+		{
+			factory->CreateButtons(testFrame);
+			int buttonId = factory->calcButtons[13]->GetId();
+			Assert::AreEqual(10013, buttonId);
+		}
+		TEST_METHOD(EnsureButtonIdOfClearIsCorrect) //10
+		{
+			factory->CreateButtons(testFrame);
+			int buttonId = factory->calcButtons[18]->GetId();
+			Assert::AreEqual(10018, buttonId);
+		}
+		
 	};
 }
