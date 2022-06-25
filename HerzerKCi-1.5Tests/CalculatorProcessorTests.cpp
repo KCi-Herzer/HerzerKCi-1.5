e53d2d4fd944b95e9a55603999631f021c3e0ceb
KCi Herzer <neonhippie.kci@gmail.com>
2022-06-24 20:22:15 -0400

    Finished Implementing Tests

--- HerzerKCi-1.5Tests/CalculatorProcessorTests.cpp
+++ HerzerKCi-1.5Tests/CalculatorProcessorTests.cpp
@@ -15,45 +15,120 @@ namespace HerzerKCi15Tests
 		{
 			testProcessor->SetBaseNumber(4);
 			testProcessor->SetBaseNumber(5);
-			std::string testString = "45";
+			double testNumber = 45;
+			std::string testString = std::to_string(testNumber);
 			testProcessor->SetOperationId(0);
 			Assert::AreEqual(testString, testProcessor->DoMath());
 		}
-		TEST_METHOD(TestMethod2)
+		TEST_METHOD(TestTwoPlusTwo)
 		{
-
+			testProcessor->ResetProcessor();
+			testProcessor->SetBaseNumber(2);
+			testProcessor->SetFirstNumber();
+			testProcessor->SetBaseNumber(2);
+			testProcessor->SetOperationId(12);
+			double testNumber = 4;
+			std::string testString = std::to_string(testNumber);
+			Assert::AreEqual(testString, testProcessor->DoMath());
 		}
-		TEST_METHOD(TestMethod3)
+		TEST_METHOD(Test432Minus400)
 		{
-
+			testProcessor->ResetProcessor();
+			testProcessor->SetBaseNumber(4);
+			testProcessor->SetBaseNumber(3);
+			testProcessor->SetBaseNumber(2);
+			testProcessor->SetFirstNumber();
+			testProcessor->SetBaseNumber(4);
+			testProcessor->SetBaseNumber(0);
+			testProcessor->SetBaseNumber(0);
+			testProcessor->SetOperationId(13);
+			double testNumber = 32;
+			std::string testString = std::to_string(testNumber);
+			Assert::AreEqual(testString, testProcessor->DoMath());
 		}
-		TEST_METHOD(TestMethod4)
+		TEST_METHOD(Test17Times38)
 		{
-
+			testProcessor->ResetProcessor();
+			testProcessor->SetBaseNumber(1);
+			testProcessor->SetBaseNumber(7);
+			testProcessor->SetFirstNumber();
+			testProcessor->SetBaseNumber(3);
+			testProcessor->SetBaseNumber(8);
+			testProcessor->SetOperationId(14);
+			double testNumber = 646;
+			std::string testString = std::to_string(testNumber);
+			Assert::AreEqual(testString, testProcessor->DoMath());
 		}
-		TEST_METHOD(TestMethod5)
+		TEST_METHOD(Test400Div80)
 		{
-
+			testProcessor->ResetProcessor();
+			testProcessor->SetBaseNumber(4);
+			testProcessor->SetBaseNumber(0);
+			testProcessor->SetBaseNumber(0);
+			testProcessor->SetFirstNumber();
+			testProcessor->SetBaseNumber(8);
+			testProcessor->SetBaseNumber(0);
+			testProcessor->SetOperationId(15);
+			double testNumber = 5;
+			std::string testString = std::to_string(testNumber);
+			Assert::AreEqual(testString, testProcessor->DoMath());
 		}
-		TEST_METHOD(TestMethod6)
+		TEST_METHOD(Test942Mod12)
 		{
-
+			testProcessor->ResetProcessor();
+			testProcessor->SetBaseNumber(9);
+			testProcessor->SetBaseNumber(4);
+			testProcessor->SetBaseNumber(2);
+			testProcessor->SetFirstNumber();
+			testProcessor->SetBaseNumber(1);
+			testProcessor->SetBaseNumber(2);
+			testProcessor->SetOperationId(16);
+			double testNumber = 6;
+			std::string testString = std::to_string(testNumber);
+			Assert::AreEqual(testString, testProcessor->DoMath());
 		}
-		TEST_METHOD(TestMethod7)
+		TEST_METHOD(Test942asHex)
 		{
-
+			testProcessor->ResetProcessor();
+			testProcessor->SetBaseNumber(9);
+			testProcessor->SetBaseNumber(4);
+			testProcessor->SetBaseNumber(2);
+			testProcessor->SetOperationId(17);
+			std::string testString = " = 0x3AE";
+			Assert::AreEqual(testString, testProcessor->DoMath());
 		}
-		TEST_METHOD(TestMethod8)
+		TEST_METHOD(Test7854asHex)
 		{
-
+			testProcessor->ResetProcessor();
+			testProcessor->SetBaseNumber(7);
+			testProcessor->SetBaseNumber(8);
+			testProcessor->SetBaseNumber(5);
+			testProcessor->SetBaseNumber(4);
+			testProcessor->SetOperationId(17);
+			std::string testString = " = 0x1EAE";
+			Assert::AreEqual(testString, testProcessor->DoMath());
 		}
-		TEST_METHOD(TestMethod9)
+		TEST_METHOD(Test2319asBin)
 		{
-
+			testProcessor->ResetProcessor();
+			testProcessor->SetBaseNumber(2);
+			testProcessor->SetBaseNumber(3);
+			testProcessor->SetBaseNumber(1);
+			testProcessor->SetBaseNumber(9);
+			testProcessor->SetOperationId(20);
+			std::string testString = " = 00000000000000000000100100001111";
+			Assert::AreEqual(testString, testProcessor->DoMath());
 		}
-		TEST_METHOD(TestMethod10)
+		TEST_METHOD(Test1738asBin)
 		{
-
+			testProcessor->ResetProcessor();
+			testProcessor->SetBaseNumber(1);
+			testProcessor->SetBaseNumber(7);
+			testProcessor->SetBaseNumber(3);
+			testProcessor->SetBaseNumber(8);
+			testProcessor->SetOperationId(20);
+			std::string testString = " = 00000000000000000000011011001010";
+			Assert::AreEqual(testString, testProcessor->DoMath());
 		}
 	};
 }
